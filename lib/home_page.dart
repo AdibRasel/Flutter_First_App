aff5fad2922ce370fd8d7fe828adf292c43192fc
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2023-11-01 07:12:22 +0600

    Lesson 6: Flutter Widgets (5+)
    Lesson 6: Flutter Widgets (5+)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,14 +16,37 @@ class HomePages extends State<HomePage> {
         title: Text("This is my First App"),
         centerTitle: true,
         leading: Icon(Icons.arrow_back),
-        actions: [
+        actions: const [
           Icon(Icons.phone_android),
           Padding(
-            padding: const EdgeInsets.only(right:10.0),
+            padding: EdgeInsets.only(right: 10.0),
             child: Icon(Icons.home),
           ),
         ],
       ),
+      body: Center(
+        child: Container(
+          height: 200.0,
+          width: 300.0,
+          color: Colors.deepOrangeAccent,
+          child: Center(
+            child: Text("This is Container Box", style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0            
+            ),),
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+
+
+
+
     );
   }
 }
